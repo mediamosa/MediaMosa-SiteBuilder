cb7a7980d055f298c6695e7ffac8ac3a4a7e09a6
Robert Blanker <rblanker@xs4all.nl>
2015-09-29 15:20:16 +0200

    Added disabled for upload button once the button is pressed.

--- sites/all/modules/custom/mediamosa_sb/mediamosa_sb.js
+++ sites/all/modules/custom/mediamosa_sb/mediamosa_sb.js
@@ -29,5 +29,23 @@ Drupal.behaviors.hideSubmitBlockit = {
   }
 };
 
-})(jQuery, window);
+Drupal.behaviors.mmsbDisableSubmitButton = {
+  attach: function(context) {
+    $('form.node-form', context).once('mmsbDisableSubmitButton', function () {
+      var $form = $(this);
+      $form.find('input.form-submit').click(function (e) {
+        var el = $(this);
+        el.after('<input type="hidden" name="' + el.attr('name') + '" value="' + el.attr('value') + '" />');
+        return true;
+      });
+      $form.submit(function (e) {
+        if (!e.isPropagationStopped()) {
+          $('input.form-submit', $(this)).attr('disabled', 'disabled');
+          return true;
+        }
+      });
+    });
+  }
+};
 
+})(jQuery, window);
